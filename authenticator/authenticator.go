376d2728d78662b081a12ae9ec9d31dbad8d4f2c
NNagato <darkdragon236@gmail.com>
2019-04-16 15:23:35 +0700

    improve authenticator constructor

--- authenticator/authenticator.go
+++ authenticator/authenticator.go
@@ -1,22 +1,30 @@
 package authenticator
 
 import (
+	"errors"
+	"strings"
+
 	"github.com/gin-contrib/httpsign"
 	"github.com/gin-contrib/httpsign/crypto"
 	"github.com/gin-contrib/httpsign/validator"
 )
 
-// NewAuthenticator create a httpsign.Authenticator to check the message signing is valid or not
-func NewAuthenticator(readKeyID, readKeySecret, writeKeyID, writeKeySecret string) *httpsign.Authenticator {
-	var (
-		secrets      = make(httpsign.Secrets)
-		mapKeySecret = map[string]string{
-			readKeyID:  readKeySecret,
-			writeKeyID: writeKeySecret,
-		}
-	)
+// KeyPair includes key and secretKey with format accessKeyID:secretAccessKey
+type KeyPair string
 
-	for key, secret := range mapKeySecret {
+// NewAuthenticator create a httpsign.Authenticator to check the message signing is valid or not
+func NewAuthenticator(keyPairs ...KeyPair) (*httpsign.Authenticator, error) {
+	var secrets = make(httpsign.Secrets)
+
+	if len(keyPairs) == 0 {
+		return nil, errors.New("keyPairs are required")
+	}
+
+	for _, keyPair := range keyPairs {
+		key, secret, err := ParseKeyPair(keyPair)
+		if err != nil {
+			return nil, err
+		}
 		signKeyID := httpsign.KeyID(key)
 		secrets[signKeyID] = &httpsign.Secret{
 			Key:       secret,
@@ -34,5 +42,24 @@ func NewAuthenticator(readKeyID, readKeySecret, writeKeyID, writeKeySecret strin
 			[]string{"(request-target)", "nonce", "digest"},
 		),
 	)
-	return auth
+	return auth, nil
+}
+
+// ParseKeyPair parse keyPair to accessKeyID and secretAccessKey
+func ParseKeyPair(keyPair KeyPair) (accessKeyID string, secretAccessKey string, err error) {
+	kp := string(keyPair)
+	if len(kp) == 0 {
+		return "", "", errors.New("missing access key keyPair")
+	}
+	keys := strings.Split(kp, ":")
+	if len(keys) != 2 {
+		return "", "", errors.New("invalid key pair format")
+	}
+	if len(keys[0]) == 0 {
+		return "", "", errors.New("missing access key id")
+	}
+	if len(keys[1]) == 0 {
+		return "", "", errors.New("missing secret access key")
+	}
+	return keys[0], keys[1], nil
 }
